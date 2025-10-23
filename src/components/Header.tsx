a5765e5087fac3184911b2e4f530ce3ca3615b3f
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-10-23 20:14:50 +0530

    Dropdown for cart items and total price display

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,65 @@
+import { useState } from "react";
 import { FaShoppingCart } from "react-icons/fa";
 import { useCart } from "../context/CartContext";
 
 const Header = () => {
+  const [showDropdown, setShowDropdown] = useState<boolean>(false);
+
   const { cart } = useCart();
   const itemCount = cart.reduce((acc, item) => acc + item.qty, 0);
 
+  const totalPrice = cart
+    .reduce((acc, item) => acc + item.price * item.qty, 0)
+    .toFixed(2);
   return (
     <header className='bg-white shadow-md p-6 flex justify-between items-center'>
       <h1 className='text-3xl font-bold text-blue-600'>Not Amazon</h1>
       <div className='relative'>
-        <FaShoppingCart className='text-2xl text-gray-700' />
-        {itemCount > 0 && (
-          <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full'>
-            {itemCount}
-          </span>
+        <button
+          className='cursor-pointer'
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          <FaShoppingCart className='text-2xl text-gray-700' />
+          {itemCount > 0 && (
+            <span className='absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full'>
+              {itemCount}
+            </span>
+          )}
+        </button>
+
+        {showDropdown && (
+          <div className='absolute right-0 mt-2 w-80 bg-white border rounded shadow-lg z-50'>
+            <div className='p-4'>
+              <h2 className='font-semibold text-lg mb-2'>Cart Items</h2>
+
+              {cart.length === 0 ? (
+                <p className='text-gray-500 text-sm'>Your cart is empty</p>
+              ) : (
+                <>
+                  <ul className='max-h-60 overflow-y-auto divide-y divide-gray-200'>
+                    {cart.map((item) => (
+                      <li
+                        key={item.id}
+                        className='flex justify-between items-center py-2'
+                      >
+                        <div>
+                          <p className='font-semibold'>{item.name}</p>
+                          <p className='text-sm text-gray-500'>
+                            {item.qty} x {item.price}
+                          </p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <div className='mt-4 flex justify-between font-semibold'>
+                    <span>Total: </span>
+                    <span>${totalPrice}</span>
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
         )}
       </div>
     </header>
